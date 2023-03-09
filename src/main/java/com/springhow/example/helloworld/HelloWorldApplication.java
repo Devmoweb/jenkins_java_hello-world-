592f504aa4c46254eea76bd98549bb186f22136a
Harsh Prajapati <devops.moweb@gmail.com>
2023-03-09 17:46:58 +0530

    new content added

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World! ~ Harsh Prajapati";
+        return "Hello World! ~ Harsh Prajapati ~ updated";
     }
 
 }
